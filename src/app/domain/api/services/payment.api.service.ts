8e7c78279e7635908951903de1267bdcc276baf9
LucasAlt40 <lucas.alt40@gmail.com>
2025-06-18 22:17:52 -0300

    POC: Implementação do form de pagamento

---
+++ src/app/domain/api/services/payment.api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LinkResponse, RequestCheckout } from '../../model/payment.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaymentApiService {
+  private readonly apiUrl = 'http://localhost:8080/checkout';
+
+  constructor(private http: HttpClient) {}
+
+  public sendCheckout(
+    requestCheckout: RequestCheckout
+  ): Observable<LinkResponse> {
+    return this.http.post<LinkResponse>(`${this.apiUrl}/new`, requestCheckout);
+  }
+}
