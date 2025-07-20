45f61c0abb9ea7c914bc4f4422538a8312829f63
Eduardo Pires Carvalho <epc44129@gmail.com>
2025-07-20 17:11:03 -0300

    feat: implementa home

--- src/app/presentation/pages/user/home-page/home-page.component.ts
+++ src/app/presentation/pages/user/home-page/home-page.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, signal, Signal } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'primeng/carousel';
 import { HttpParams } from '@angular/common/http';
@@ -19,10 +25,19 @@ export class HomePageComponent implements OnInit {
   responsiveOptions: any[] | undefined;
   imagesBaseUrl: string = environment.imagesBaseUrl;
 
+  categories = signal<any[]>([]);
+
+  categorizedEvents: WritableSignal<{ category: string; events: Event[] }[]> =
+    signal([]);
+
   constructor(private eventService: EventApiService) {}
 
   ngOnInit() {
     this.loadEvents(1, this.eventsHighlighted);
+    this.loadCategories();
+
+    console.log('Events', this.categorizedEvents);
+
     this.responsiveOptions = [
       {
         breakpoint: '1400px',
@@ -55,6 +70,39 @@ export class HomePageComponent implements OnInit {
 
     this.eventService.findAll(params).subscribe((events) => {
       items.set(events.content.filter((event) => event.previewImageUrl));
+    });
+  }
+  private loadCategories() {
+    this.eventService.getAllEventCategories().subscribe((response) => {
+      this.categories.set(response.content);
+      console.log('Categories loaded:', this.categories());
+      this.loadCategorizedEvents();
+    });
+  }
+
+  private loadCategorizedEvents() {
+    this.categories().forEach((category) => {
+      console.log('category', category.id);
+      const params = new HttpParams()
+        .set('page', '0')
+        .set('pageSize', '5')
+        .set('categoryId', category.id.toString());
+
+      this.eventService.findAll(params).subscribe((response) => {
+        const filtered = response.content.filter(
+          (event) => event.previewImageUrl
+        );
+
+        this.categorizedEvents.update((current) => [
+          ...current,
+          {
+            category: category.name,
+            events: filtered,
+          },
+        ]);
+
+        console.log(this.categorizedEvents());
+      });
     });
   }
 }
