d0c7f3910194d093776cddc5570322fc0ac130be
LizzBricce <bricce.e@aluno.ifsp.edu.br>
2025-07-20 22:42:32 -0300

    fix: corrigindo validação

--- src/app/presentation/pages/admin/add-ticket-category-page/add-ticket-category-page.component.ts
+++ src/app/presentation/pages/admin/add-ticket-category-page/add-ticket-category-page.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+
 import { EventApiService } from '../../../../core/api/services/event.api.service';
+
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
+
+import { ToastModule } from 'primeng/toast';            
+import { MessageService } from 'primeng/api';          
 
 @Component({
   selector: 'app-add-ticket-category-page',
@@ -18,7 +23,9 @@ import { ButtonModule } from 'primeng/button';
     InputTextModule,
     InputNumberModule,
     ButtonModule,
+    ToastModule,  
   ],
+  providers: [MessageService],  
   templateUrl: './add-ticket-category-page.component.html',
 })
 export class AddTicketCategoryPageComponent {
@@ -30,19 +37,25 @@ export class AddTicketCategoryPageComponent {
 
   constructor(
     private eventService: EventApiService,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService     
   ) {}
 
   ngOnInit() {
     this.eventService.getEvents().subscribe({
       next: (res) => {
         this.events = res.content.map((event: any) => ({
-          id: (event as any).id,
-          title: (event as any).title,
+          id: event.id,
+          title: event.title,
         }));
       },
       error: () => {
-        console.error('Erro ao carregar eventos');
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao carregar eventos',
+          life: 5000,
+        });
       },
     });
   }
@@ -54,7 +67,12 @@ export class AddTicketCategoryPageComponent {
       this.ticketCategoryQuantity === null ||
       this.ticketCategoryPrice === null
     ) {
-      alert('Preencha todos os campos!');
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Atenção',
+        detail: 'Preencha todos os campos!',
+        life: 5000,
+      });
       return;
     }
 
@@ -67,11 +85,21 @@ export class AddTicketCategoryPageComponent {
       })
       .subscribe({
         next: () => {
-          alert('Categoria de ingresso criada com sucesso!');
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: 'Categoria de ingresso criada com sucesso!',
+            life: 5000,
+          });
           this.router.navigate(['/admin/categorias-evento']);
         },
         error: () => {
-          alert('Erro ao criar categoria de ingresso!');
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao criar categoria de ingresso!',
+            life: 5000,
+          });
         },
       });
   }
