2dd34502db4bfc1f96f9020fc62bca6d98f6dbf3
caioslopes <caiolopes.social@gmail.com>
2025-07-20 22:20:33 -0300

    feat: edicao de evento

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  imagesBaseUrl: 'http://10.242.21.192:9000/selene/public/'
+  imagesBaseUrl: 'http://10.242.21.192:9000/selene/public/',
 };
