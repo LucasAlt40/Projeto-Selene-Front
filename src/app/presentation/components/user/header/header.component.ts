9a1f93e39b77cac2bbb9c4c017d9ed0d828f86e4
caioslopes <caiolopes.social@gmail.com>
2025-07-21 22:25:43 -0300

    style: header

--- src/app/presentation/components/user/header/header.component.ts
+++ src/app/presentation/components/user/header/header.component.ts
@@ -1,28 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { InputText } from 'primeng/inputtext';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { AuthService } from '../../../../core/services/auth.service';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [IconFieldModule, InputIconModule, InputText, RouterModule],
+  standalone: true,
+  imports: [MenuModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  isAuthenticated: boolean = false;
+  isAuthenticated = false;
   user: any = null;
+  items: MenuItem[] = [];
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.isAuthenticated = this.authService.isAuthenticated();
     this.user = this.authService.getUser();
+
+    this.items = [
+      {
+        label: 'Meu pedidos',
+        icon: 'pi pi-user-edit',
+        command: () => this.router.navigate(['/pedidos']),
+      },
+      {
+        label: 'Sair',
+        icon: 'pi pi-sign-out',
+        command: () => this.logout(),
+      },
+    ];
   }
 
   logout() {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
